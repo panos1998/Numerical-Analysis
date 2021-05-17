2de30d8e0669453d63cc066b4ba629414125a12c
panos1998 <petridisp@ece.auth.gr>
2021-05-17 19:02:37 +0300

    log log diagramm

--- app1.py
+++ app1.py
@@ -1,12 +1,13 @@
 import math
+
 from IntegralMethods.RectangleIntegration import leftrectangle, rightrectangle
 from IntegralMethods.TrapezoidIntegration import trapezoid
-from math import sin, exp, pi, log
+from  math import sin, exp, pi
 import numpy as np
-
-leftrect = {}
-rightrect = {}
-trapez = {}
+import matplotlib.pyplot as plt
+leftrect=[]
+rightrect=[]
+trapez=[]
 sol = (2/13)+(3/13*np.e**(3/4*np.pi))
 step = [10**-1, 10**-2, 10**-3, 10**-4, 10**-5, 10**-6, 10**-7]
 def f(x):
@@ -16,9 +17,11 @@ def f(x):
 
 if __name__ == "__main__":
     for i in step:
-     leftrect[math.log10(i)] = math.log10(abs(sol - leftrectangle(0, pi/4, i, f)))
-     rightrect[math.log10(i)] = math.log10(abs(sol - rightrectangle(0, pi/4, i, f)))
-     trapez[math.log10(i)] = math.log10(abs(sol - trapezoid(0, pi/4, i, f)))
+     leftrect.append(math.log10(abs(sol - leftrectangle(0, pi/4, i, f))))
+     rightrect.append(math.log10(abs(sol - rightrectangle(0, pi/4, i, f))))
+     trapez.append(math.log10(abs(sol - trapezoid(0, pi/4, i, f))))
     print(leftrect)
     print(rightrect)
     print(trapez)
+    plt.plot(leftrect,step)
+    plt.show()
