bf62ac9dfd970b35247c87fb9e15670f4b2feccd
panos1998 <petridisp@ece.auth.gr>
2021-05-17 18:24:39 +0300

    log log dicts

--- app1.py
+++ app1.py
@@ -1,10 +1,12 @@
+import math
 from IntegralMethods.RectangleIntegration import leftrectangle, rightrectangle
 from IntegralMethods.TrapezoidIntegration import trapezoid
-from math import sin, exp, pi
+from math import sin, exp, pi, log
 import numpy as np
-leftrect=[]
-rightrect=[]
-trapez=[];
+
+leftrect = {}
+rightrect = {}
+trapez = {}
 sol = (2/13)+(3/13*np.e**(3/4*np.pi))
 step = [10**-1, 10**-2, 10**-3, 10**-4, 10**-5, 10**-6, 10**-7]
 def f(x):
@@ -14,9 +16,9 @@ def f(x):
 
 if __name__ == "__main__":
     for i in step:
-     leftrect.append(abs(sol - leftrectangle(0, pi/4, i, f)))
-     rightrect.append(abs(sol - rightrectangle(0, pi/4, i, f)))
-     trapez.append(abs(sol - trapezoid(0, pi/4, i, f)))
+     leftrect[math.log10(i)] = math.log10(abs(sol - leftrectangle(0, pi/4, i, f)))
+     rightrect[math.log10(i)] = math.log10(abs(sol - rightrectangle(0, pi/4, i, f)))
+     trapez[math.log10(i)] = math.log10(abs(sol - trapezoid(0, pi/4, i, f)))
     print(leftrect)
     print(rightrect)
     print(trapez)
