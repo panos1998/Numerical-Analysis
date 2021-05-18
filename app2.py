c872c35d8221c8a4c23f3c06ef33f6be0ebbbe21
panos1998 <petridisp@ece.auth.gr>
2021-05-18 22:00:17 +0300

    simpson plus fix

--- app2.py
+++ app2.py
@@ -7,19 +7,20 @@ F = lambda t, x: np.array([1.1 * x[0] - 0.4 * x[0] * x[1], 0.4 * x[0] * x[1] - 0
 x0 = [20, 1]
 t0 = 0
 tmax = 200
-dt = 0.01
-t, x = euler(F, x0, t0, tmax, dt)
+dt = [0.1,0.01]
+for dt in dt:
+ t, x = euler(F, x0, t0, tmax, dt)
 
-print(t,x)
+ print(t,x)
 
-plt.figure(1)
-plt.plot(t,x[:, 0],"b-",t,x[:, 1],"r--")
-plt.grid()
-plt.title("Time Evolution of Foxes Population and Rabbits Population")
-plt.legend(["Rabbits","Foxes"])
-plt.xlabel("time")
-plt.ylabel("No. of Rabbits and No. of Foxes")
-plt.show()
+ plt.figure(1)
+ plt.plot(t,x[:, 0],"b-",t,x[:, 1],"r--")
+ plt.grid()
+ plt.title("Time Evolution of Foxes Population and Rabbits Population")
+ plt.legend(["Rabbits","Foxes"])
+ plt.xlabel("time")
+ plt.ylabel("No. of Rabbits and No. of Foxes")
+ plt.show()
 
 plt.figure(2)
 plt.plot(x[:, 0], x[:, 1], "k--")
